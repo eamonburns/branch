333c7f1932d5f8c020c9ce880238993ff63150b1
Eamon Burns <eamon.r.burns@gmail.com>
2026-07-14 11:43:09 -0700

    feat(lua): add `opts.name` to `Cmd`

--- lua/branch.lua
+++ lua/branch.lua
@@ -7,89 +7,97 @@ local branch = {}
 ---@param opts { url: string, name?: string, key?: string }
 ---@return branch.Item
 function branch.Site(opts)
-	if type(opts) ~= "table" then
-		error("Site: opts is not a table", 2)
-	end
+  if type(opts) ~= "table" then
+    error("Site: opts is not a table", 2)
+  end
 
-	local url = opts.url
-	if type(url) ~= "string" then
-		error("Site: opts.url is not a string", 2)
-	end
+  local url = opts.url
+  if type(url) ~= "string" then
+    error("Site: opts.url is not a string", 2)
+  end
 
-	local self = {
-		url = url,
-	}
+  local self = {
+    url = url,
+  }
 
-	return _branch_new_item(opts.name or url, opts.key, function()
-		_branch_open_url(self.url)
-	end)
+  return _branch_new_item(opts.name or url, opts.key, function()
+    _branch_open_url(self.url)
+  end)
 end
 
----@param opts { command: string, arguments?: string[], key?: string }
+---@param opts { command: string, arguments?: string[], name?: string, key?: string }
 ---@return branch.Item
 function branch.Cmd(opts)
-	if type(opts) ~= "table" then
-		error("Cmd: opts is not a table", 2)
-	end
+  if type(opts) ~= "table" then
+    error("Cmd: opts is not a table", 2)
+  end
 
-	if type(opts.command) ~= "string" then
-		error("Cmd: opts.command is not a string", 2)
-	end
+  if type(opts.command) ~= "string" then
+    error("Cmd: opts.command is not a string", 2)
+  end
 
-	opts.arguments = opts.arguments or {}
+  opts.arguments = opts.arguments or {}
 
-	if type(opts.arguments) ~= "table" then
-		error("Cmd: opts.arguments is not a list of strings", 2)
-	end
-	for _, a in ipairs(opts.arguments) do
-		if type(a) ~= "string" then
-			error("Cmd: opts.arguments is not a list of strings", 2)
-		end
-	end
+  if type(opts.arguments) ~= "table" then
+    error("Cmd: opts.arguments is not a list of strings", 2)
+  end
+  for _, a in ipairs(opts.arguments) do
+    if type(a) ~= "string" then
+      error("Cmd: opts.arguments is not a list of strings", 2)
+    end
+  end
 
-	local name = table.concat({ opts.command, unpack(opts.arguments) }, " ")
+  if opts.name and type(opts.name) ~= "string" then
+    error("Cmd: opts.name is not a string", 2)
+  end
 
-	local self = {
-		command = opts.command,
-		arguments = opts.arguments,
-	}
+  if opts.key and type(opts.key) ~= "string" then
+    error("Cmd: opts.key is not a string", 2)
+  end
 
-	return _branch_new_item(name, opts.key, function()
-		_branch_exec(self.command, unpack(self.arguments))
-	end)
+  local name = opts.name or table.concat({ opts.command, unpack(opts.arguments) }, " ")
+
+  local self = {
+    command = opts.command,
+    arguments = opts.arguments,
+  }
+
+  return _branch_new_item(name, opts.key, function()
+    _branch_exec(self.command, unpack(self.arguments))
+  end)
 end
 
 ---@param opts { title: string, key?: string, [integer]: branch.Item }
 ---@return branch.Item
 function branch.Menu(opts)
-	if type(opts) ~= "table" then
-		error("Menu: opts is not a table", 2)
-	end
+  if type(opts) ~= "table" then
+    error("Menu: opts is not a table", 2)
+  end
 
-	local title = opts.title
-	if type(title) ~= "string" then
-		error("Menu: opts.title is not a string", 2)
-	end
+  local title = opts.title
+  if type(title) ~= "string" then
+    error("Menu: opts.title is not a string", 2)
+  end
 
-	if not opts[1] then
-		error("Menu: no menu items", 2)
-	end
+  if not opts[1] then
+    error("Menu: no menu items", 2)
+  end
 
-	return _branch_new_menu(title, opts.key, unpack(opts))
+  return _branch_new_menu(title, opts.key, unpack(opts))
 end
 
 ---@param opts { name: string, key?: string }
 ---@return branch.Item
 function branch.None(opts)
-	if type(opts) ~= "table" then
-		error("None: opts is not a table", 2)
-	end
+  if type(opts) ~= "table" then
+    error("None: opts is not a table", 2)
+  end
 
-	local name = opts.name
-	if type(name) ~= "string" then
-		error("None: opts.name is not a string", 2)
-	end
+  local name = opts.name
+  if type(name) ~= "string" then
+    error("None: opts.name is not a string", 2)
+  end
 
-	return _branch_new_none(name, opts.key)
+  return _branch_new_none(name, opts.key)
 end
 _G.branch = branch
--- lua/zig_bindings.lua
+++ lua/zig_bindings.lua
@@ -5,7 +5,7 @@
 ---@param activate fun() # Function to be called when activated
 ---@return branch.Item
 function _branch_new_item(name, key, activate) ---@diagnostic disable-line: unused-local
-	error("_branch_new_item should be defined by host")
+  error("_branch_new_item should be defined by host")
 end
 
 ---@param name string # Display name
@@ -13,23 +13,23 @@ end
 ---@param ... branch.Item # List of items in the menu
 ---@return branch.Item
 function _branch_new_menu(name, key, ...) ---@diagnostic disable-line: unused-local,unused-vararg
-	error("_branch_new_menu should be defined by host")
+  error("_branch_new_menu should be defined by host")
 end
 
 ---@param name string # Display name
 ---@param key? string # Key to activate item
 ---@return branch.Item
 function _branch_new_none(name, key) ---@diagnostic disable-line: unused-local
-	error("_branch_new_none should be defined by host")
+  error("_branch_new_none should be defined by host")
 end
 
 ---@param url string # URL to open
 function _branch_open_url(url) ---@diagnostic disable-line: unused-local
-	error("_branch_open_url should be defined by host")
+  error("_branch_open_url should be defined by host")
 end
 
 ---@param command string # Command to run
 ---@param ... string # Arguments to pass to command
 function _branch_exec(command, ...) ---@diagnostic disable-line: unused-local,unused-vararg
-	error("_branch_exec should be defined by host")
+  error("_branch_exec should be defined by host")
 end
